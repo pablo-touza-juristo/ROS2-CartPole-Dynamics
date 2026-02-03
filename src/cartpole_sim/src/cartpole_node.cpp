b39fbe4244cd97ad6d855beec5693a1363c0e57b
pablo-touza-juristo <tjpablo05@gmail.com>
2026-02-03 11:00:41 +0100

    chore: add README file for documentation of the project

--- src/cartpole_sim/src/cartpole_node.cpp
+++ src/cartpole_sim/src/cartpole_node.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 #include <rclcpp/rclcpp.hpp>
 #include <Eigen/Dense>
 int main()
 {
+  Eigen::Matrix4d test_matrix 
+  {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}
+  };
+
+  std::cout << "Test Matrix: " << test_matrix << std::endl; 
+
   return 0;
 }
