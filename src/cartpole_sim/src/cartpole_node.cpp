8f210163964d486da2b235377af1279cb125afb5
pablo-touza-juristo <tjpablo05@gmail.com>
2026-02-16 11:36:12 +0100

    feat: update CMakeLists.txt for correct project compilation and create the file for simple_pendulum implementation

---
+++ src/cartpole_sim/src/cartpole_node.cpp
@@ -0,0 +1,3 @@
+#include "include/dynamics/simple_pendulum"
+
+int main() { return (0); }
---
+++ src/cartpole_sim/src/dynamics/simple_pendulum.cpp
@@ -0,0 +1,3 @@
+#include "../../include/cartpole_sim/dynamics/simple_pendulum.hpp"
+
+namespace cartpole_sim::dynamics {}  // namespace cartpole_sim::dynamics
